ca4a67f862ee51264564851050dd79c006b0a9f4
PozhilayaKrevetka192 <7545245@ukr.net>
2022-10-18 18:48:10 +0300

    trying to fix new error

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,7 +9,7 @@ module.exports = authMiddleWare = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]
         if (!token) {
-            res.json({message: "User is not authorized"})
+            res.status(400).json({message: "User is not authorized"})
         }
         
         const user = jwt.verify(token, secret)
