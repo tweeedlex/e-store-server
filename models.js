c8a002ddbb0154b812b4256e0d428ddde387fe56
PozhilayaKrevetka192 <7545245@ukr.net>
2022-11-06 20:30:01 +0200

    fix

--- models.js
+++ models.js
@@ -18,7 +18,7 @@ const BasketItem = sequelize.define("basket_item", {
 
 const Item = sequelize.define("item", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
     info: {type: DataTypes.JSON},
     img: {type: DataTypes.STRING, allowNull: false}
